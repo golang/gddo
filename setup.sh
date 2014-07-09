536879dfcf2669b7b2ef986c9f774d692520dc10
Francesc Campoy <campoy@golang.org>
2014-07-09 16:48:47 -0700

    talksapp: adapt to the present reorganization
    reviewed https://codereview.appspot.com/106580045/

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 go get code.google.com/p/go.tools/godoc
-present=`go list -f '{{.Dir}}' code.google.com/p/go.talks/present`
+present=`go list -f '{{.Dir}}' code.google.com/p/go.tools/cmd/present`
 godoc=`go list -f '{{.Dir}}' code.google.com/p/go.tools/godoc`
 mkdir -p present
 
 (cat $godoc/static/jquery.js $godoc/static/playground.js $godoc/static/play.js && echo "initPlayground(new HTTPTransport());") > present/play.js
 
-cd present
+cd ./present
 for i in templates static
 do
     ln -is $present/$i
