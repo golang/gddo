340040c94ad83bce0a376071c167782e39c42f4d
Francesc Campoy <campoy@golang.org>
2015-07-01 12:13:12 -0700

    removing unwanted change

--- lintapp/main.go
+++ lintapp/main.go
@@ -33,7 +33,7 @@ func init() {
 }
 
 var (
-	contactEmail    = "unknown@example.com"
+	contactEmail    = "golang-dev@googlegroups.com"
 	homeTemplate    = parseTemplate("common.html", "index.html")
 	packageTemplate = parseTemplate("common.html", "package.html")
 	errorTemplate   = parseTemplate("common.html", "error.html")
