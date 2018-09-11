golang/gddo

commit 9d8ff1c67be5c2da6321c656913e1fe355991138

improve redirection to canonical import path
Problem:

Our GitHub organisation is named Teamwork, but for various hard to
change reasons we import packages as teamwork (lower case t). To enforce
this we add an import enforcement for our packages:

	package duck // import "github.com/teamwork/duck"

This works well, but godoc.org enforces users to use the canonical
GitHub path (Teamwork), but with the import statement the Go compiler
enforces a different path, leading to an error.

Fix:

Instead of immediately redirecting in the gosrc package, it will now
only store the canonical import path there. This is before the Go source
code is actually scanned, so we don't *actually* know what the real
canonical path should be.

In the doc.newPackage() function it will check for the canonical import
path, taking both the Go import path as well as the reported path from
the gosrc package in to account, and redirect as needed.

This seems to work for all the cases I can think of:

    github.com/Teamwork/validate    -> github.com/teamwork/validate
    github.com/pkg/ERRORS           -> github.com/pkg/errors
    github.com/Carpetsmoker/sconfig -> arp242.net/sconfig
    arp242.net/SCONFIG              -> not found (expected)
    bitbucket.org/pkg/inflect       -> works
    bitbucket.org/pkg/INFLECT       -> works (should probably redirect too)
    github.com/docker/docker/client -> works (#534)
    github.com/moby/moby/client     -> redirects (on master this actually seems to error out?)

It should also be easy to add a similar check for for some other repo
providers, such as BitBucket, GitLab, etc.

Fixes #507

Change-Id: I49c5ccb328694f95dd7a0e5d577297d56d88893f
GitHub-Last-Rev: 28efc3756488fdd07649986f26afcbe85385bd5d
GitHub-Pull-Request: golang/gddo#560
Reviewed-on: https://go-review.googlesource.com/120059
Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

doc/builder.go
@@ -576,10 +576,23 @@ func newPackage(dir *gosrc.Directory) (*Package, error) {
 		return pkg, nil
 	}
 
-	if bpkg.ImportComment != "" && bpkg.ImportComment != dir.ImportPath {
-		return nil, gosrc.NotFoundError{
-			Message:  "not at canonical import path",
-			Redirect: bpkg.ImportComment,
+	switch {
+	case bpkg.ImportComment != "":
+		// Redirect to import path comment.
+		if dir.ImportPath != bpkg.ImportComment {
+			return nil, gosrc.NotFoundError{
+				Message:  "not at canonical import path",
+				Redirect: bpkg.ImportComment,
+			}
+		}
+	case bpkg.ImportComment == "" && dir.ResolvedGitHubPath != "":
+		// Make sure to redirect to GitHub's reported canonical casing when
+		// there's no import path comment.
+		if dir.ImportPath != dir.ResolvedGitHubPath {
+			return nil, gosrc.NotFoundError{
+				Message:  "not at canonical import path",
+				Redirect: dir.ResolvedGitHubPath,
+			}
 		}
 	}
 

gosrc/gosrc.go
@@ -55,6 +55,13 @@ type Directory struct {
 	// Import path of package after resolving go-import meta tags, if any.
 	ResolvedPath string
 
+	// Import path of package with the canonical user/repo case as reported by
+	// the github.com server. Optional.
+	// If set, used to ensure canonical case is used when there's no import path
+	// comment (e.g., to redirect from "github.com/UsEr/rEpO/dir" to
+	// "github.com/User/Repo/dir").
+	ResolvedGitHubPath string
+
 	// Import path prefix for all packages in the project.
 	ProjectRoot string
 

gosrc/github.go
@@ -36,7 +36,6 @@ func init() {
 var (
 	gitHubRawHeader     = http.Header{"Accept": {"application/vnd.github-blob.raw"}}
 	gitHubPreviewHeader = http.Header{"Accept": {"application/vnd.github.preview"}}
-	ownerRepoPat        = regexp.MustCompile(`^https://api.github.com/repos/([^/]+/[^/]+)/`)
 )
 
 type githubCommit struct {
@@ -63,6 +62,7 @@ func getGitHubDir(ctx context.Context, client *http.Client, match map[string]str
 	c := &httpClient{client: client, errFn: gitHubError}
 
 	var repo struct {
+		FullName      string    `json:"full_name"`
 		Fork          bool      `json:"fork"`
 		Stars         int       `json:"stargazers_count"`
 		CreatedAt     time.Time `json:"created_at"`
@@ -126,10 +126,6 @@ func getGitHubDir(ctx context.Context, client *http.Client, match map[string]str
 		return nil, NotFoundError{Message: "No files in directory."}
 	}
 
-	if err := validateGitHubProjectName(contents[0].GitURL, match); err != nil {
-		return nil, err
-	}
-
 	var files []*File
 	var dataURLs []string
 	var subdirs []string
@@ -158,41 +154,20 @@ func getGitHubDir(ctx context.Context, client *http.Client, match map[string]str
 	}
 
 	return &Directory{
-		BrowseURL:      browseURL,
-		Etag:           commits[0].ID,
-		Files:          files,
-		LineFmt:        "%s#L%d",
-		ProjectName:    match["repo"],
-		ProjectRoot:    expand("github.com/{owner}/{repo}", match),
-		ProjectURL:     expand("https://github.com/{owner}/{repo}", match),
-		Subdirectories: subdirs,
-		VCS:            "git",
-		Status:         status,
-		Fork:           repo.Fork,
-		Stars:          repo.Stars,
+		ResolvedGitHubPath: "github.com/" + repo.FullName + match["dir"],
+		BrowseURL:          browseURL,
+		Etag:               commits[0].ID,
+		Files:              files,
+		LineFmt:            "%s#L%d",
+		ProjectName:        match["repo"],
+		ProjectRoot:        expand("github.com/{owner}/{repo}", match),
+		ProjectURL:         expand("https://github.com/{owner}/{repo}", match),
+		Subdirectories:     subdirs,
+		VCS:                "git",
+		Status:             status,
+		Fork:               repo.Fork,
+		Stars:              repo.Stars,
 	}, nil
-}
-
-// validateGitHubProjectName checks if the requested owner and repo names match
-// the case of the canonical names returned by Github. GitHub owner and repo names
-// are case-insensitive so they must be normalized to the canonical casing.
-//
-// Returns a not found error if the names are the same, but have different case.
-// Returns nil if the names match exactly, or if the names are different,
-// which will happen if the url is a github redirect to a new project name.
-func validateGitHubProjectName(canonicalName string, requestMatch map[string]string) error {
-	m := ownerRepoPat.FindStringSubmatch(canonicalName)
-	if m == nil {
-		return nil
-	}
-	requestedName := requestMatch["owner"] + "/" + requestMatch["repo"]
-	if m[1] == requestedName || !strings.EqualFold(m[1], requestedName) {
-		return nil
-	}
-	return NotFoundError{
-		Message:  "Github import path has incorrect case.",
-		Redirect: fmt.Sprintf("github.com/%s%s", m[1], requestMatch["dir"]),
-	}
 }
 
 // isQuickFork reports whether the repository is a "quick fork":

commit ee3b7b11ec6a84e3826e396cbc3a4176e2a759fc

doc: don't redirect vanity packages to GitHub
This change fixes a regression from CL 120059. We started to redirect
to GitHub's reported canonical case when the request path didn't
match too aggressively. It should only be done if the request path is
already on GitHub; otherwise we're redirecting a vanity import path to
its GitHub hosting location whenever it doesn't have an import path
comment.

Add tests for this newPackage redirection behavior to increase
confidence in the fix, and help avoid future regressions.

Fix typos in gosrc package comments.

Fixes golang/gddo#579.

Change-Id: I7ed0787a125d2f39ca979d01983e971729e181fd
Reviewed-on: https://go-review.googlesource.com/134355
Reviewed-by: Tuo Shan <shantuo@google.com>

doc/builder.go
@@ -579,16 +579,20 @@ func newPackage(dir *gosrc.Directory) (*Package, error) {
 
 	switch {
 	case bpkg.ImportComment != "":
-		// Redirect to import path comment.
+		// Redirect to import path comment, if not already there.
 		if dir.ImportPath != bpkg.ImportComment {
 			return nil, gosrc.NotFoundError{
 				Message:  "not at canonical import path",
 				Redirect: bpkg.ImportComment,
 			}
 		}
-	case bpkg.ImportComment == "" && dir.ResolvedGitHubPath != "":
-		// Make sure to redirect to GitHub's reported canonical casing when
-		// there's no import path comment.
+
+	// Redirect to GitHub's reported canonical casing when there's no import path comment,
+	// and the import path differs from resolved GitHub path only in case.
+	case bpkg.ImportComment == "" && dir.ResolvedGitHubPath != "" &&
+		strings.EqualFold(dir.ImportPath, dir.ResolvedGitHubPath):
+
+		// Redirect to resolved GitHub path, if not already there.
 		if dir.ImportPath != dir.ResolvedGitHubPath {
 			return nil, gosrc.NotFoundError{
 				Message:  "not at canonical import path",

gosrc/gosrc.go
@@ -455,7 +455,7 @@ func getDynamic(ctx context.Context, client *http.Client, importPath, etag strin
 	return dir, nil
 }
 
-// getStatic gets a diretory from a statically known service. getStatic
+// getStatic gets a directory from a statically known service. getStatic
 // returns errNoMatch if the import path is not recognized.
 func getStatic(ctx context.Context, client *http.Client, importPath, etag string) (*Directory, error) {
 	for _, s := range services {

doc/builder_test.go
@@ -9,6 +9,8 @@ package doc
 import (
 	"go/ast"
 	"testing"
+
+	"github.com/golang/gddo/gosrc"
 )
 
 var badSynopsis = []string{
@@ -109,6 +111,111 @@ func TestSimpleImporter(t *testing.T) {
 		obj, _ := simpleImporter(m, tt.path)
 		if obj.Name != tt.name {
 			t.Errorf("simpleImporter(%q) = %q, want %q", tt.path, obj.Name, tt.name)
+		}
+	}
+}
+
+// TestNewPackageRedirect tests that newPackage redirects
+// and does not redirect as expected, in various situations.
+// See https://github.com/golang/gddo/issues/507
+// and https://github.com/golang/gddo/issues/579.
+func TestNewPackageRedirect(t *testing.T) {
+	// robpike.io/ivy package.
+	// Vanity import path, hosted on GitHub, with import path comment.
+	ivy := gosrc.Directory{
+		Files: []*gosrc.File{
+			{Name: "main.go", Data: []byte("package main // import \"robpike.io/ivy\"\n")},
+		},
+		ResolvedGitHubPath: "github.com/robpike/ivy",
+	}
+
+	// go4.org/sort package.
+	// Vanity import path, hosted on GitHub, without import path comment.
+	go4sort := gosrc.Directory{
+		Files: []*gosrc.File{
+			{Name: "main.go", Data: []byte("package sort\n")},
+		},
+		ResolvedGitHubPath: "github.com/go4org/go4/sort",
+	}
+
+	// github.com/teamwork/validate package.
+	// Hosted on GitHub, with import path comment that doesn't match canonical GitHub case.
+	// See issue https://github.com/golang/gddo/issues/507.
+	gtv := gosrc.Directory{
+		Files: []*gosrc.File{
+			{Name: "main.go", Data: []byte("package validate // import \"github.com/teamwork/validate\"\n")},
+		},
+		ResolvedGitHubPath: "github.com/Teamwork/validate", // Note that this differs from import path comment.
+	}
+
+	tests := []struct {
+		name         string
+		repo         gosrc.Directory
+		requestPath  string
+		wantRedirect string // Empty string means no redirect.
+	}{
+		// ivy.
+		{
+			repo: ivy, name: "ivy repo: access canonical path -> no redirect",
+			requestPath: "robpike.io/ivy",
+		},
+		{
+			repo: ivy, name: "ivy repo: access GitHub path -> redirect to import comment",
+			requestPath:  "github.com/robpike/ivy",
+			wantRedirect: "robpike.io/ivy",
+		},
+		{
+			repo: ivy, name: "ivy repo: access GitHub path with weird casing -> redirect to import comment",
+			requestPath:  "github.com/RoBpIkE/iVy",
+			wantRedirect: "robpike.io/ivy",
+		},
+
+		// go4sort.
+		{
+			repo: go4sort, name: "go4sort repo: access canonical path -> no redirect",
+			requestPath: "go4.org/sort",
+		},
+		{
+			repo: go4sort, name: "go4sort repo: access GitHub path -> no redirect",
+			requestPath: "github.com/go4org/go4/sort",
+		},
+		{
+			repo: go4sort, name: "go4sort repo: access GitHub path with weird casing -> redirect to resolved GitHub case",
+			requestPath:  "github.com/gO4oRg/Go4/sort",
+			wantRedirect: "github.com/go4org/go4/sort",
+		},
+
+		// gtv.
+		{
+			repo: gtv, name: "gtv repo: access canonical path -> no redirect",
+			requestPath: "github.com/teamwork/validate",
+		},
+		{
+			repo: gtv, name: "gtv repo: access canonical GitHub path -> redirect to import comment",
+			requestPath:  "github.com/Teamwork/validate",
+			wantRedirect: "github.com/teamwork/validate",
+		},
+		{
+			repo: gtv, name: "gtv repo: access GitHub path with weird casing -> redirect to import comment",
+			requestPath:  "github.com/tEaMwOrK/VaLiDaTe",
+			wantRedirect: "github.com/teamwork/validate",
+		},
+	}
+	for _, tt := range tests {
+		dir := tt.repo
+		dir.ImportPath = tt.requestPath
+
+		var want error
+		if tt.wantRedirect != "" {
+			want = gosrc.NotFoundError{
+				Message:  "not at canonical import path",
+				Redirect: tt.wantRedirect,
+			}
+		}
+
+		_, got := newPackage(&dir)
+		if got != want {
+			t.Errorf("%s: got error %v, want %v", tt.name, got, want)
 		}
 	}
 }

gosrc/gosrc_test.go
@@ -22,7 +22,7 @@ import (
 var testWeb = map[string]string{
 	// Package at root of a GitHub repo.
 	"https://alice.org/pkg": `<head> <meta name="go-import" content="alice.org/pkg git https://github.com/alice/pkg"></head>`,
-	// Package in sub-diretory.
+	// Package in sub-directory.
 	"https://alice.org/pkg/sub": `<head> <meta name="go-import" content="alice.org/pkg git https://github.com/alice/pkg"><body>`,
 	// Fallback to http.
 	"http://alice.org/pkg/http": `<head> <meta name="go-import" content="alice.org/pkg git https://github.com/alice/pkg">`,